f265a530bce3b91fd402b18cd5661d236292315f
bhavesh357 <bkadam357@gmail.com>
2020-08-17 02:13:15 +0530

    ADDED: feature to load data from api

--- src/js/script.js
+++ src/js/script.js
@@ -33,3 +33,58 @@ function toggleTools(){
 function toggleDropdown(dropdown,value){
     dropdown.style.top=value;
 }
+
+let listFlag=true;
+let greetingList=document.getElementById('greeting-list');
+let greetingListItems=document.getElementById('greeting-list-items-list');
+
+function showList(){
+    console.log(greetingList);
+    if(listFlag){
+        loadGreetingData();
+        greetingList.style.display='block';
+        listFlag=false;
+    }else{
+        greetingList.style.display='none';
+        listFlag=true;
+    }
+}
+
+function loadGreetingData(){
+    fetch('http://localhost:3000/greetings')
+    .then(data => {return data.json()})
+    .then(res => {
+        console.log(res);
+        loadData(res);
+    })
+    .catch(err => console.log(err));
+}
+
+function loadData(data){
+    greetingListItems.innerHTML="";
+    console.log(data);
+    console.log(data[2]);
+    for(let i=0;i<data.length;i++){
+        console.log(i);
+        console.log(data[i]);
+        let item="<div class=\"col-md-4\"><div class=\"col-md-12  greeting-list-item\"><div class=\"greeting-list-item-btn greeting-list-item-edit\">"+
+                "<img class=\"greeting-list-item-btn\" src=\"./img/icons/pen.svg\">"+
+            "</div>"+
+                "<div class=\"greeting-list-item-id\">ObjectId("+data[i]._id+")</div>"+
+            "<div class=\"greeting-list-item-greeting greeting-list-item-data\">"+
+                "<div class=\"greeting-list-item-greeting-text\">"+data[i].message+"</div>"+
+                "<div class=\"greeting-list-item-greeting-info\">(Greeting)</div>"+
+            "</div>"+
+            "<div class=\"greeting-list-item-name greeting-list-item-data\">"+
+                "<div class=\"greeting-list-item-greeting-text\">"+data[i].firstName+" "+data[i].lastName+"</div>"+
+                "<div class=\"greeting-list-item-greeting-info\">(Name)</div>"+
+            "</div>"+
+            "<div class=\"greeting-list-item-time\">15 mins ago</div>"+
+            "<div class=\"greeting-list-item-btn greeting-list-item-delete\">"+
+                "<img  class=\"greeting-list-item-btn\" src=\"./img/icons/delete.svg\">"+
+            "</div>"+
+            "</div>"+
+        "</div>";
+        greetingListItems.innerHTML+=item;
+    }
+}
